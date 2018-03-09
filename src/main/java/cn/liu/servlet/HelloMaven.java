6f0404d9b8086c5ac615cfcab5847961d8233876
Braw115 <liuxueliang115@163.com>
2018-03-09 16:47:24 +0800

    Initial commit

---
+++ src/main/java/cn/liu/servlet/HelloMaven.java
@@ -0,0 +1,19 @@
+package cn.liu.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloMaven extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write("Hello Maven");
+    }
+
+
+}
